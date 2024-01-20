ffd60f08b95ec61fc827d4657fca1f5d9f16eb83
Alvaro C <alvarogco97@gmail.com>
2024-01-20 12:13:20 -0500

    Types of data

---
+++ data_types.py
@@ -0,0 +1,41 @@
+#Tipos de datos en Python 
+#Python es un leguaje dinamicamente tipado
+
+#1. númericos
+#Enteros
+num1 = 40
+print("Num1: ", type(num1))
+#Flotantes o decimales (reales) => float
+num2= 50.5
+print("Num2: ", type(num2))
+#Números complejos (Complex)
+num3= 2j
+print("Num3: ", type(num3))
+#2. Cadena
+my_name = "Alvaro"
+print("my_name", type(my_name))
+my_lastname = 'Castro'
+print("my_lastname", type(my_lastname))
+profile = '''Alvaro es una persona enfocada 
+en el trabajo de grupos'''
+print("profile", type(profile))
+#3. Lógicos (valores o expresiones booleanos)
+usuario_activo = True
+print("usuario_activo", type(usuario_activo))
+pago_realiado = False
+print("pago_realiado", type(pago_realiado))
+#4. Listas (se caracterisan por estar entre corchetes [])
+
+frutas = ['apple', 'banana']
+colors = ['blue','red', 'orange']
+detodito = ["Alvaro", 26, 50000, 'Pasto', 1.70, 27.0]
+
+print(frutas)
+print("frutas", type(frutas))
+print(colors)
+print("frutas", type(colors))
+print(detodito)
+print("detodito", type(detodito[0]))
+#5. Tuplas 
+#6. Diccionarios
+#7. Conjuntos 
