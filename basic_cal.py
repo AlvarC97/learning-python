5940766ea1542e4eba6a50a30be06d178926f211
Alvaro C <alvarogco97@gmail.com>
2024-01-20 09:47:08 -0500

    Add coge

---
+++ basic_cal.py
@@ -0,0 +1,26 @@
+'''Script title: Basic calculator v1 
+Depeloper name: Alvaro G. Castro
+Date: 18-01-2024'''
+'''Description 
+This calculator let you get the result of math basic math operations
+from two numbers (variables)'''
+#Calculadora Básica 
+
+#Inputs
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+#Process
+suma = num1 + num2 
+resta = num1 - num2 
+multiplicacion = num1 * num2 
+division = num1 / num2 
+#Outputs
+print("La suma es =", suma)
+print("La resta es = ", resta)
+print("La multiplicación es = ", multiplicacion)
+print("La division es =", division)
+print("La segunda forma es así:")
+print("La suma es =", num1 + num2)
+print("La resta es = ", num1 - num2)
+print("La multiplicación es = ", num1 * num2)
+print("La division es =", num1 / num2)
