dae7acaba318f99a86647e136abb7ffe1b3f2507
Alvaro C <alvarogco97@gmail.com>
2024-01-23 12:05:18 -0500

    Upload two

---
+++ welcome2.py
@@ -0,0 +1,8 @@
+'''Una función se crea con la palabra reservda
+def '''
+
+def saludo():  #Esta es una función y 2 puntos para tabular
+    print("Hola, bienvenidos")
+    print("Mi nombre es Álvaro")
+#Llama o invoca la función
+saludo()
