1580031cc5b19e0e3aebbdb728d2ba04a3b13075
Alvaro C <alvarogco97@gmail.com>
2024-01-23 12:06:53 -0500

    Upload two

--- aleatorios1.py
+++ aleatorios1.py
@@ -1,5 +1,9 @@
 import os  #Para limpiar pantalla
 from random import randint, uniform
+'''
+randint => Genera número aleatorios enteros entre un rango especificado 
+uniform => Genera números aleatorios flotantes o decimales en un rango especificado 
+'''
 
 os.system('clear')
 
